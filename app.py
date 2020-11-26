871760ee23ef40df67f837de3116e095b17f7578
ayeffkay <chatton1@mail.ru>
2020-11-26 13:06:17 +0300

    Translation changed and cache included

--- app.py
+++ app.py
@@ -3,24 +3,17 @@ import redis
 import speech2text
 import translate
 import pronounce
-from collections import OrderedDict
 
 
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 db = redis.StrictRedis(host='redis',
                        port=6379, db=0, charset='utf-8', decode_responses=True)
-app.config['last_id'] = max(map(int, db.keys())) if len(db.keys()) else 0
 
 
 @app.route("/")
 def index():
-    history = OrderedDict(zip(['text', 'translation'], [[], []]))
-    for key in sorted(map(int, db.keys())):
-        for name, value in db.hgetall(key).items():
-            history[name].append(value)
-    history = {field: '\n'.join(texts).strip() for field, texts in history.items()}
-    return render_template('index.html', data=history)
+    return render_template('index.html')
 
 
 @app.route("/clear-history")
@@ -30,23 +23,33 @@ def clear_history():
 
 
 def add_row(text, translation):
-    app.config['last_id'] += 1
-    new_id = str(app.config['last_id'])
     text = text.split('\n')[-1]
     translation = translation.split('\n')[-1]
-    db.hmset(new_id, {'text': text, 'translation': translation})
+    db.mset({text: translation})
     pronounce.text2speech(translation)
+
+
+def make_translation(text):
+    loaded_from_cache = False
+    last = text.split('\n')[-1]
+    if db.exists(last):
+        translation = db.get(last)
+        loaded_from_cache = True
+    else:
+        translation = translate.get_translation(text)
+        if translation != 'Not recognized':
+            # for simplicity one text row of textarea is considered as one query and the last sentence saved
+            add_row(text, translation)
+    return translation, loaded_from_cache
 
 
 @app.route('/translate-text', methods=['POST'])
 def translate_text():
     data = request.get_json()
     text = data['text']
-    translation = translate.get_translation(text)
-    # for simplicity one text row of textarea is considered as one query and the last sentence saved
-    add_row(text, translation)
+    translation, cache_flag = make_translation(text)
 
-    return jsonify(text=text, translation=translation)
+    return jsonify(text=text, translation=translation, cache_flag=cache_flag)
 
 
 @app.route("/translate-audio", methods=['POST'])
@@ -55,10 +58,9 @@ def translate_audio():
     with open('audio.wav', 'wb') as audio:
         f.save(audio)
     text = speech2text.text_from_audio('audio.wav')
-    translation = translate.get_translation(text)
-    add_row(text, translation)
+    translation, cache_flag = make_translation(text)
 
-    return jsonify(text=text, translation=translation)
+    return jsonify(text=text, translation=translation, cache_flag=cache_flag)
 
 
 @app.route('/play-audio')
--- translate.py
+++ translate.py
@@ -1,10 +1,14 @@
 from googletrans import Translator
+import time
 
 
-def get_translation(text):
+def get_translation(text, n_queries=5):
     translator = Translator()
-    try:
-        res = translator.translate(text=text, src='ru', dest='en')
-        return res.text
-    except:
-        return 'Not recognized'
+    for _ in range(n_queries):
+        try:
+            res = translator.translate(text=text, src='ru', dest='en')
+            return res.text
+        except:
+            time.sleep(0.2)
+    return 'Not recognized'
+
